f3bc1368b2100b9dbad82c01cfb7b93bf5587336
Fer <fervelalcantara@gmail.com>
2022-06-30 16:12:47 -0500

    comentario

--- index.php
+++ index.php
@@ -8,18 +8,20 @@
             <img src="auto.ico" alt="" width="30" height="24" class="d-inline-block align-text-top">
             ASEGURADORA
         </a>
+        <form class="d-flex" action="index.php" method="POST"  name="buscar">
         <nav class="navbar navbar-light bg-light">
             <div class="container-fluid">
                 
-                <form class="d-flex" action="index.php" method="POST"  name="buscar">
+                
                     <input id="txtBuscador" class="form-control me-2" type="text" placeholder="Poliza"
                         aria-label="Search" name="buscador">
                     <button class="btn btn-outline-secondary" type="button"> <img id="img1" src="lupa.ico" alt=""
                             width="25" height="25" class="d-inline-block align-text-top" /></button>
-                </form>
-
+               
+        
             </div>
         </nav>
+        </form>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
             aria-controls="offcanvasNavbar">
             <span class="navbar-toggler-icon"></span>
@@ -33,6 +35,7 @@
                     aria-label="Close"></button>
             </div>
             <?php if(isset($_SESSION['message'])){?>
+                
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                         <?= $_SESSION['message']?>
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -117,7 +120,7 @@
                         
                     <?php   ?>
                         <?php
-
+                            //Holaa
                                 if(isset($_POST['guardar'])) {
                                     //$buscar = $_POST['buscador'];
                                     $query = "SELECT
